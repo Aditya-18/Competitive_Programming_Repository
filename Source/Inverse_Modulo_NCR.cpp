2c082f84439d89c6709e841b17c59d84e9203daf
Aditya-18 <adityagoel1997.ag@gmail.com>
2017-09-04 18:45:26 +0530

    Create Inverse_Modulo_NCR.cpp
    This will compute the inverse modulo and ncr for the given MOD in O(N)

---
+++ Source/Inverse_Modulo_NCR.cpp
@@ -0,0 +1,15 @@
+//Uses the efficient power function that we already build
+
+ll fact[SIZE], inv[SIZE];
+void build_inverse_modulo() {
+    assert(MOD >= SIZE);
+    fact[0] = 1;
+    rep(i, 1, SIZE) 
+		fact[i] = MUL(fact[i-1], i);
+    inv[SIZE - 1] = power(fact[SIZE - 1], MOD - 2, MOD);
+    for(int i = SIZE - 2; i >= 0; i--) inv[i] = MUL(inv[i + 1], (i + 1));
+}
+
+int ncr(int n, int r) {
+    return MUL(MUL(fact[n], inv[n - r]), inv[r]);
+}
