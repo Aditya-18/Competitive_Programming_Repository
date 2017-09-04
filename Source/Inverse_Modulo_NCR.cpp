03680e878542c8ed7facf78fc05d8a68668ff7e4
Aditya-18 <adityagoel1997.ag@gmail.com>
2017-09-04 18:52:46 +0530

    Update Inverse_Modulo_NCR.cpp

--- Source/Inverse_Modulo_NCR.cpp
+++ Source/Inverse_Modulo_NCR.cpp
@@ -7,7 +7,7 @@ void build_inverse_modulo() {
     assert(MOD >= SIZE);
     fact[0] = 1;
     rep(i, 1, SIZE) 
-		fact[i] = MUL(fact[i-1], i);
+	fact[i] = MUL(fact[i-1], i);
     inv[SIZE - 1] = power(fact[SIZE - 1], MOD - 2, MOD);
     for(int i = SIZE - 2; i >= 0; i--) inv[i] = MUL(inv[i + 1], (i + 1));
 }
